b019d00767ad1eac6abbf39c5a3ecc8e8ae35ba8
dario <dariopizzi12@gmail.com>
2022-03-21 20:20:35 -0300

    cambios

--- app/Http/Controllers/ControladorCliente.php
+++ app/Http/Controllers/ControladorCliente.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Entidades\Sistema\Cliente; //include_once "app/Entidades/Sistema/Menu.php";
 use App\Entidades\Sistema\Patente;
 use App\Entidades\Sistema\Usuario;
+use App\Entidades\Pais;
 use Illuminate\Http\Request;
+
 
 require app_path() . '/start/constants.php';
 
@@ -13,7 +15,9 @@ class ControladorCliente extends Controller
 {
       public function nuevo(){
             $titulo = "Nuevo cliente";
-            return view("cliente.cliente-nuevo", compact('titulo'));
+            $pais = new Pais();
+            $array_nacionalidad = $pais->obtenerTodos();
+            return view("cliente.cliente-nuevo", compact('titulo', 'array_nacionalidad'));
       }
 
 }
