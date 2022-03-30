a7bac0c22268e4f81b76c46832a1592056e85888
ntarche <nelson.tarche@gmail.com>
2022-03-30 15:38:37 -0300

    cambios

--- resources/views/web/plantilla.blade.php
+++ resources/views/web/plantilla.blade.php
@@ -85,22 +85,10 @@
                     <nav class="nav-menu">
                         <ul>
                             <li class="active"><a href="/">Inicio</a></li>
-                            <li><a href="./about-us.html">About Us</a></li>
-                            <li><a href="./class-details.html">Classes</a></li>
-                            <li><a href="./services.html">Services</a></li>
-                            <li><a href="./team.html">Our Team</a></li>
-                            <li><a href="#">Pages</a>
-                                <ul class="dropdown">
-                                    <li><a href="./about-us.html">About us</a></li>
-                                    <li><a href="./class-timetable.html">Classes timetable</a></li>
-                                    <li><a href="./bmi-calculator.html">Bmi calculate</a></li>
-                                    <li><a href="./team.html">Our team</a></li>
-                                    <li><a href="./gallery.html">Gallery</a></li>
-                                    <li><a href="./blog.html">Our blog</a></li>
-                                    <li><a href="./404.html">404</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="./contact.html">Contact</a></li>
+                            <li><a href="/nosotros">Nosotros</a></li>
+                            <li><a href="/profesores">Profesores</a></li>
+                            <li><a href="/clases">Clases</a></li>
+                            <li><a href="/contacto">Contacto</a></li>
                         </ul>
                     </nav>
                 </div>
