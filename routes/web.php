857839d41f2186ddbef907baa4718c7280f3db83
agustinAffricani <agustin.affricani@gmail.com>
2022-03-30 18:55:43 -0300

    cambios

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::group(array('domain' => '127.0.0.1'), function () {
 
     Route::get('/', 'ControladorWebHome@index');
     Route::get('/nosotros', 'ControladorWebNosotros@index');
+    Route::get('/contacto', 'ControladorWebContacto@index');
  
 
     Route::get('/admin', 'ControladorHome@index');
