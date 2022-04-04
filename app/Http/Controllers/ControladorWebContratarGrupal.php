fc80fb0584c71bf89b6b0b5c1eff88e51665980d
ntarche <nelson.tarche@gmail.com>
2022-04-04 20:58:45 -0300

    cambios

--- app/Http/Controllers/ControladorWebContratarGrupal.php
+++ app/Http/Controllers/ControladorWebContratarGrupal.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers;
 use App\Entidades\Cliente;
 use Illuminate\Http\Request;
 use Session;
+use MercadoPago\Item;
+use MercadoPago\MerchantOrder;
+use MercadoPago\Payer;
+use MercadoPago\Payment;
+use MercadoPago\Preference;
+use MercadoPago\SDK;
+
 
 class ControladorWebContratarGrupal extends Controller
 {
