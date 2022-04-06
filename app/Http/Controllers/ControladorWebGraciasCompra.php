45d32ef427423f87cf16dd4ea976ca4502891374
dario <dariopizzi12@gmail.com>
2022-04-06 19:45:55 -0300

    cambios

---
+++ app/Http/Controllers/ControladorWebGraciasCompra.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ControladorWebGraciasCompra extends Controller
+{
+    public function index()
+    {
+        return view("web.gracias-compra");
+    }
+}
---
+++ resources/views/web/gracias-compra.blade.php
@@ -0,0 +1,18 @@
+@extends('web.plantilla')
+@section('contenido')
+<section class="contact-section spad">
+      <div class="container">
+            <div class="row">
+                  <div class="col-12">
+                        <div class="section-title h2 mt-5">
+                              <h2>Gracias por su compra!</h2>
+                        </div>
+                  </div>
+            </div>
+            <div class="row spad">
+                  <div class="col-12">
+                        <h3>Vamos a confirmar que el pago haya llegado correctamente y le va a estar llegando un correo con los links de acceso para las clases grupales</h3>
+                  </div>
+            </div>
+      </div>
+</section>
